9caa94d85e24ee26796749ab8eec966e77b0f07c
maheshwaribnd <mamtabnd097@gmail.com>
2023-01-08 11:05:16 +0530

    updated

--- index.js
+++ index.js
@@ -33,3 +33,5 @@ AOS.init({
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
+
+
